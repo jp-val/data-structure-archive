01c083ef59a67b6bb61be91a29e4ff73cb43f3e2
jp val <valdespino.jp@knights.ucf.edu>
2021-08-17 01:38:58 -0400

    bloom filters

---
+++ BloomFilter_v1.java
@@ -0,0 +1,85 @@
+// JP Valdespino
+// August 2021
+
+// BloomFilter_v1.java
+// ================
+// Version 1: Fast implimentation, but heavy on memory. Instead of having different 
+// hash functions, we can use mutiple tables of different lengths.
+
+import java.util.*;
+import java.math.*;
+
+public class BloomFilter_v1
+{
+	private boolean[][] table; // Our Bloom Filter.
+	private int size; // The number of strings that have been inserted.
+
+	public BloomFilter_v1()
+	{
+		// Given these table sizes, we can insert about 500 thousand strings before we start getting
+		// too many collisions which would intefere with the data strcuture's effectiveness.
+		int[] sizes = { 1000003, 1000033, 1000037, 1000039, 1000081, 1000099, 1000117 };
+
+		this.table = new boolean[sizes.length][];
+
+		for (int i = 0; i < this.table.length; i++)
+			this.table[i] = new boolean[sizes[i]];
+
+		this.size = 0;
+	}
+
+	public BloomFilter_v1(int[] sizes)
+	{
+		this.table = new boolean[sizes.length][];
+
+		for (int i = 0; i < this.table.length; i++)
+			this.table[i] = new boolean[sizes[i]];
+
+		this.size = 0;
+	}
+
+	public void add(String str)
+	{
+		int hashValue = str.hashCode(); // Integer overflow occurs, but it doesn't matter.
+
+		for (int i = 0; i < this.table.length; i++)
+		{
+			int j = Math.abs(hashValue%this.table[i].length);
+			this.table[i][j] = true;
+		}
+
+		this.size++;
+	}
+
+	// Bloom Filters do not supposrt deletion.
+	public boolean remove(String str)
+	{
+		return false;
+	}
+
+	public boolean contains(String str)
+	{
+		int hashValue = str.hashCode();
+
+		for (int i = 0; i < this.table.length; i++)
+		{
+			int j = Math.abs(hashValue%this.table[i].length);
+			if (!this.table[i][j]) return false;
+		}
+
+		return true;
+	}
+
+	public static void main(String[] args)
+	{
+		BloomFilter_v1 t = new BloomFilter_v1();
+		
+		t.add("university");
+		System.out.println(t.contains("university"));
+		System.out.println(t.contains("powerful"));
+
+		t.add("flamethrower");
+		System.out.println(t.contains("flamethrower"));
+		System.out.println(t.contains("firepower"));
+	}
+}
---
+++ BloomFilter_v2.java
@@ -0,0 +1,67 @@
+// JP Valdespino
+// August 2021
+
+// BloomFilter_v2.java
+// ===================
+// Version 2: Using bitmasks along with BigInteger we can save a lot of memory compared 
+// to using boolean arrays.
+
+import java.util.*;
+import java.math.*;
+
+public class BloomFilter_v2
+{
+	private int[] sizes = { 982451549, 982451567, 982451579, 982451581, 982451609, 982451629, 982451653 };
+	private BigInteger[] table;
+	private int size;
+
+	public BloomFilter_v2()
+	{
+		table = new BigInteger[this.sizes.length];
+		Arrays.fill(table, BigInteger.ZERO);
+	}
+
+	public void add(String str)
+	{
+		int hashValue = str.hashCode();
+		for (int i = 0; i < this.sizes.length; i++)
+		{
+			int j = Math.abs(hashValue%this.sizes[i]);
+			this.table[i] = this.table[i].setBit(j);
+		}
+
+		this.size++;
+	}
+
+	// Bloom Filters do not supposrt deletion.
+	public boolean remove(String str)
+	{
+		return false;
+	}
+
+	public boolean contains(String str)
+	{
+		int hashValue = str.hashCode();
+		for (int i = 0; i < this.sizes.length; i++)
+		{
+			int j = Math.abs(hashValue%this.sizes[i]);
+			if (!this.table[i].testBit(j)) return false;
+		}
+
+		return true;
+	}
+
+	public static void main(String[] str)
+	{
+		int[] sizes =  { 5, 7, 11, 13, 17, 19, 23 };
+		BloomFilter_v2 t = new BloomFilter_v2();
+
+		t.add("university");
+		t.add("flamethrower");
+
+		System.out.println(t.contains("powerful"));
+		System.out.println(t.contains("helpful"));
+		System.out.println(t.contains("university"));
+		System.out.println(t.contains("flamethrower"));
+	}
+}
